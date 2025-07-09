5a3f5502275a8f6eaa818eda99d3c058348abe8d
atingupta2006 <65483056+atingupta2006@users.noreply.github.com>
2025-07-09 15:57:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ provider "aws" {
 
 variable "elb_names" {
   type = list
-  default = ["dev-loadbalancer", "stage-loadbalanacer","prod-loadbalancer"]
+  default = ["ating-dev-loadbalancer", "ating-stage-loadbalanacer","ating-gh-prod-loadbalancer"]
 }
+
 
 resource "aws_iam_user" "lb" {
   name = var.elb_names[count.index]
