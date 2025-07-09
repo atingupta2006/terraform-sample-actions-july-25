6a46985d4b11780bc6cda709308c60c6b2da890d
Atin Gupta <atingupta@email.com>
2025-07-09 14:24:20 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+}
+
+variable "elb_names" {
+  type = list
+  default = ["dev-loadbalancer", "stage-loadbalanacer","prod-loadbalancer"]
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.elb_names[count.index]
+  count = 3
+  path = "/system/"
+}
+
+
+output "iam_names" {
+  value = aws_iam_user.lb[*].name
+}
