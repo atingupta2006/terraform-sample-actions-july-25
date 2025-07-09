067f5a0845793e07f4c17dbc7c55bc9db9fc843f
Atin Gupta <atingupta@email.com>
2025-07-09 14:45:06 +0530

    -

--- main.tf
+++ main.tf
@@ -13,6 +13,14 @@ resource "aws_iam_use" "lb" {
 }
 
 
+resource "aws_iam_use" "lb" {
+  name = "atin"
+  count = 3
+  path = "/system/"
+}
+
+
+
 output "iam_names" {
   value = aws_iam_user.lb[*].name
 }
